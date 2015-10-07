aa12edf4e9cd880f81b16e00f5fdf13f1f1ac7e4
Arun Krishnakumar <arunxls@gmail.com>
2015-10-07 12:15:27 -0500

    Time fixed

--- InvertedLinks/InvertedLinks.cpp
+++ InvertedLinks/InvertedLinks.cpp
@@ -5,7 +5,7 @@
 #include <string>
 #include <vector>
 #include <deque>
-#include <ctime>
+#include <chrono>
 #include "include_types.h"
 #include "FileReader.h"
 #include "SplitHash.h"
@@ -35,47 +35,47 @@ void __cdecl _tmain(int argc, TCHAR *argv[])
     printf("Starting split phase\n");
     std::deque<std::string> files;
 
-    begin = clock();
+    std::chrono::high_resolution_clock::time_point b1 = std::chrono::high_resolution_clock::now();
     {
         SplitHash splitHash(argv[1], buffer_start, buffer_end);
         splitHash.execute();
         files = splitHash.merge_files;
     }
-    end = clock();
-    printf("Took %ld seconds\n", double(end - begin) / CLOCKS_PER_SEC);
+    std::chrono::high_resolution_clock::time_point e1 = std::chrono::high_resolution_clock::now();
+    printf("Took %lld seconds\n", std::chrono::duration_cast<std::chrono::seconds>(e1 - b1).count());
     printf("Generated %d files\n", files.size());
     printf("Ending split phase\n");
 
     printf("Starting merge phase\n");
-    begin = clock();
+    std::chrono::high_resolution_clock::time_point b2 = std::chrono::high_resolution_clock::now();
     {
         MergeHash merge(buffer_start, buffer_end, files);
         merge.execute();
         files = merge.merge_files;
     }
-    end = clock();
-    printf("Took %ld seconds\n", double(end - begin) / CLOCKS_PER_SEC);
+    std::chrono::high_resolution_clock::time_point e2 = std::chrono::high_resolution_clock::now();
+    printf("Took %lld seconds\n", std::chrono::duration_cast<std::chrono::seconds>(e2 - b2).count());
     printf("Ending merge phase\n");
 
     printf("Staring map split phase\n");
-    begin = clock();
+    std::chrono::high_resolution_clock::time_point b3 = std::chrono::high_resolution_clock::now();
     {
         Map map(buffer_start, buffer_end, files[0], argv[2]);
         map.execute();
         files = map.output_files;
     }
-    end = clock();
-    printf("Took %ld seconds\n", double(end - begin) / CLOCKS_PER_SEC);
+    std::chrono::high_resolution_clock::time_point e3 = std::chrono::high_resolution_clock::now();
+    printf("Took %lld seconds\n", std::chrono::duration_cast<std::chrono::seconds>(e3 - b3).count());
     printf("Ending map split phase\n");
 
     printf("Starting map merge phase\n");
-    begin = clock();
+    std::chrono::high_resolution_clock::time_point b4 = std::chrono::high_resolution_clock::now();
     {
         MapMerge mapMerge(buffer_start, buffer_end, files);
         mapMerge.execute();
     }
-    end = clock();
-    printf("Took %ld seconds\n", double(end - begin) / CLOCKS_PER_SEC);
+    std::chrono::high_resolution_clock::time_point e4 = std::chrono::high_resolution_clock::now();
+    printf("Took %lld seconds\n", std::chrono::duration_cast<std::chrono::seconds>(e4 - b4).count());
     printf("Ending map merge phase\n");
 
     printf("DONE!\n");
